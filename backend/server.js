f925633f6c87de68cce8888a3114c19015e23e7d
Abdullah <abdullahrangwala@gmail.com>
2026-06-02 15:32:08 +0530

    Final

--- backend/server.js
+++ backend/server.js
@@ -15,15 +15,7 @@ const allowedOrigins = [
   'https://sweetcounty.vercel.app/' // Replace with your actual Vercel URL later if needed
 ];
 app.use(cors({
-  origin: function (origin, callback) {
-    // allow requests with no origin (like mobile apps or curl requests)
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
+  origin: true,
   credentials: true
 }));
 app.use(express.json());
